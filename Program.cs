4ba85d8432170017e2e55d3f2ab9c03bbf26a3a1
aamahi2020 <aamahi2020@gmail.com>
2025-04-29 17:11:09 +0300

    initial setup

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace JsonExample
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string json = File.ReadAllText("user.json");
+            List<User> users = JsonConvert.DeserializeObject<List<User>>(json);
+
+            foreach (var user in users)
+            {
+                Console.WriteLine($"Name: {user.Name}");
+                Console.WriteLine($"Age: {user.Age}");
+                Console.WriteLine($"City: {user.City}");
+                Console.WriteLine("-----------------------");
+            }
+        }
+    }
+}
+
